70715c35db3598d9bd5205195da9bc0d09af254c
garycarolan <garycarolan@outlook.com>
2024-03-05 12:14:59 +0000

    New class for finding best params, removed redundant graph gen code

--- gridsearch.py
+++ gridsearch.py
@@ -26,26 +26,22 @@ def detect_conflicts(G):
             G.nodes[node]['flag'] = False
     return conflicts
 
-def resolve_conflicts(G, g_used_colors, reserve_colors):
-    # At start of iteration create copy graph to ensure synchronicity
+def resolve_conflicts(G, g_used_colors, reserve_colors, colour_introduction_chance, colour_change_chance):
     copy = G.copy()
-    colour_introduction_chance = 0.001
-    colour_change_chance = 0.5
     for node in G.nodes():
         if G.nodes[node]['flag']:
             neighbor_colors = [copy.nodes[neighbor]['color'] for neighbor in copy.neighbors(node)]
             available_colors = [color for color in g_used_colors if color not in neighbor_colors]
-            if not available_colors and random.random() < colour_introduction_chance:  #COLOUR INTRODUCTION CHANCE # if empty
+            if not available_colors and random.random() < colour_introduction_chance:
                 new_color = reserve_colors.pop()
                 g_used_colors.append(new_color)
-                G.nodes[node]['color'] = new_color  # only G, not copy is written to
-            elif random.random() < colour_change_chance:  # COLOUR CHANGE CHANCE # this was changed to be random
+                G.nodes[node]['color'] = new_color
+            elif random.random() < colour_change_chance:
                 if len(available_colors) > 0:
                     G.nodes[node]['color'] = random.choice(available_colors)
                 else:
                     G.nodes[node]['color'] = random.choice(neighbor_colors)
 
-            # Because the alg is synchronous and detect conflicts will be called not sure if this is necessary
             G.nodes[node]['flag'] = False
             copy.nodes[node]['flag'] = False
 
@@ -55,10 +51,10 @@ def draw_graph(G, pos, iteration):
     plt.show()
 
 def main():
-    num_nodes = 50
-    num_graphs = 20
+    num_nodes = 200
+    num_graphs = 1
 
-    g_used_colors = ['#fc5185', '#36486b']  # Initial list of colors
+    g_used_colors = ['#fc5185', '#36486b']
     reserve_colors = [
         '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
         '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf',
@@ -70,46 +66,43 @@ def main():
         '#fff200', '#00b2ff', '#4a4e4d', '#8a89a6', '#997c6c',
         '#3d4b52', '#5bc0eb', '#fde74c', '#9bc53d', '#c3423f',
         '#f7f4a3', '#36486b', '#3fc1c9'
-    ]  # List of reserve colors
-    estimates = []
-    used_colors_lengths = []
-    iterations = []
+    ]
 
-    for _ in range(num_graphs):
-        random_graph = generate_connected_random_graph(num_nodes, g_used_colors)
-        conflicts = detect_conflicts(random_graph)
-        iteration = 1
+    # Pre-generate graphs
+    graphs = [generate_connected_random_graph(num_nodes, g_used_colors) for _ in range(num_graphs)]
 
-        # Generate node positions with spring layout once
-        pos = nx.spring_layout(random_graph)
+    # Define parameters to search
+    param_grid = {
+        'colour_introduction_chance': [0.001, 0.005, 0.01],
+        'colour_change_chance': [0.1, 0.3, 0.5]
+    }
 
-        # Figure to compare against:
-        estimate = max(nx.greedy_color(random_graph).values()) + 1
-        #print('Minimum Colors: ' + str(estimate))
+    best_params = None
+    best_used_colors_length = float('inf') # starts with high number
 
-        while conflicts > 0:
-            #draw_graph(random_graph, pos, iteration)  # edit here to start drawing graphs ******
-            resolve_conflicts(random_graph, g_used_colors, reserve_colors)
-            conflicts = detect_conflicts(random_graph)
-            iteration += 1
+    for intro_chance in param_grid['colour_introduction_chance']:
+        for change_chance in param_grid['colour_change_chance']:
+            used_colors_lengths = []
+            for graph in graphs:
+                conflicts = detect_conflicts(graph)
+                iteration = 1
+                pos = nx.spring_layout(graph)
 
-        draw_graph(random_graph, pos, iteration)
-        used_colors_lengths.append(len(g_used_colors))
-        estimates.append(estimate)
-        iterations.append(iteration)
+                while conflicts > 0:
+                    resolve_conflicts(graph, g_used_colors, reserve_colors, intro_chance, change_chance)
+                    conflicts = detect_conflicts(graph)
+                    iteration += 1
 
-    #print('Estimates:   ', estimates)
-    #print('Used Colors: ', used_colors_lengths)
-    #print('Iterations:  ', iterations)
+                used_colors_lengths.append(len(g_used_colors))
 
-    # Calculate averages
-    avg_estimate = sum(estimates) / num_graphs
-    avg_used_colors_length = sum(used_colors_lengths) / num_graphs
-    avg_iterations = sum(iterations) / num_graphs
-    print('Average Minimum Colors:     ', avg_estimate)
-    print('Average Number Used Colors: ', avg_used_colors_length)
-    print('Average Iterations:         ', avg_iterations)
+            avg_used_colors_length = sum(used_colors_lengths) / num_graphs
+            if avg_used_colors_length < best_used_colors_length:
+                best_used_colors_length = avg_used_colors_length
+                best_params = (intro_chance, change_chance)
 
+    print('Best intro_chance:', best_params[0])
+    print('Best change_chance:', best_params[1])
+    print('Best used colors length:', best_used_colors_length)
 
 if __name__ == "__main__":
     main()
